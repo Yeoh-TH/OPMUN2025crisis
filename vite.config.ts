c683643221821fd8e14ee56a83060757a92329cd
YeohTH <soundwavedecepticonleader@gmail.com>
2025-05-25 22:14:34 +0800

    base change maybe

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/opmun2025crisis/',
+  base: '/OPMUN2025crisis/',
   plugins: [react()],
 })
